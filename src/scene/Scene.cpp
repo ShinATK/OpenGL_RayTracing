32264ae00fb4362a1bb797298a288c837437501d
ShinATK <soutakushin@gmail.com>
2024-03-07 19:48:49 +0800

    Update Scene.cpp

--- src/scene/Scene.cpp
+++ src/scene/Scene.cpp
@@ -4,10 +4,7 @@ Scene::Scene(GLFWwindow* window, const GLuint width, const GLuint height) :m_wid
 {
 	// Camera
 	m_camera = new Camera();
-	m_projection = glm::perspective(m_camera->zoom_, (float)m_width / (float)m_height, m_camera->near_, m_camera->far_);
-
-	// Player
-	m_player = new Object("player0", glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.2f));
+	m_projection = glm::perspective(m_camera->GetZoom(), (float)m_width / (float)m_height, m_camera->GetNear(), m_camera->GetFar());
 
 	// Load Default
 	ResourceManager::LoadShader("./resources/shaders/default.vert", "./resources/shaders/default.frag", "default");
@@ -47,18 +44,15 @@ void Scene::Init()
 {
 	// 加载 shader
 	ResourceManager::LoadShader("./resources/shaders/bbox.vert", "./resources/shaders/bbox.frag", "bbox");
-	ResourceManager::LoadShader("./resources/shaders/phong.vert", "./resources/shaders/phong.frag", "phong");
+	ResourceManager::LoadShader("./resources/shaders/basicLight.vert", "./resources/shaders/basicLight.frag", "basicLight");
 
 	// 加载 texture
 	ResourceManager::LoadTexture("./resources/textures/container.jpg", GL_FALSE, "container");
 	ResourceManager::LoadTexture("./resources/textures/wood.png", GL_TRUE, "wood");
-	
-	// 加载 material
-	ResourceManager::LoadMaterial("phongMaterial", "phong", "./resources/materials/default.txt");
 
 	// 创建 objects
 	Objects["Object"]["container"]   = new Object("container", glm::vec3(0, 0, -3));
-	Objects["Object"]["floor"]  = new Object("floor", glm::vec3(0, -1, 0));
+	Objects["Object"]["floor"]  = new Object("floor", glm::vec3(0, -1, 0), glm::vec3(10.0f, 1.0f, 10.0f));
 
 	// 创建 lights
 	Objects["Light"]["light"]   = new Object("light", glm::vec3(1.2f, 1.0f, 2.0f), glm::vec3(0.5f));
@@ -67,12 +61,9 @@ void Scene::Init()
 
 void Scene::Render(GLfloat dt)
 {
-
 	// Render
 	if(this->m_showSkybox) this->DrawSkybox(ResourceManager::GetShader("skybox"));
 	this->DrawObjects();
-	//this->m_player->Draw();
-
 }
 
 void Scene::ProcessInput(GLfloat dt)
@@ -85,7 +76,7 @@ void Scene::Update()
 {
 	// Update view matrix
 	glm::mat4 view    = m_camera->GetViewMatrix();
-	glm::vec3 viewPos = m_camera->position_;
+	glm::vec3 viewPos = m_camera->GetPosition();
 	// ResourceManager 自动更新使用中的shader view matrix
 	ResourceManager::UpdateShader(m_projection, view, viewPos);
 	// 防止 skybox 随着 camera 移动而移动
@@ -96,35 +87,24 @@ void Scene::Update()
 	// 更新 light 位置（light 跟随可视化光源位置的 cube 移动）
 	Lights[0]->m_position = Objects["Light"]["light"]->GetPosition();
 
-	// 更新 player 位置
-	m_player->SetPosition(m_camera->position_ + glm::normalize(m_camera->front_));
-	
-	// TODO：player 随视角转动，保证正面朝前
-	// TODO: 视角旋转时，camera应当在以player所在位置为中心，以player到camera距离为半径的球面上旋转
-	// Objects["Object"]["player"]->SetRotation();
-	// m_camera->GetRotation();
-
-
-
 	// Update Bounding Box
 	if (m_chosenObject != nullptr)
 		m_chosenObject->Get2DBBox(m_projection, view, m_width, m_height);
 
 	// Update light uniform
-	ResourceManager::GetShader("phong").SetVector3f("light.position",	Lights[0]->m_position);
-	ResourceManager::GetShader("phong").SetVector3f("light.ambient",	Lights[0]->m_ambient);
-	ResourceManager::GetShader("phong").SetVector3f("light.diffuse",	Lights[0]->m_diffuse);
-	ResourceManager::GetShader("phong").SetVector3f("light.specular",	Lights[0]->m_specular);
+	ResourceManager::GetShader("basicLight").SetVector3f("light.position",	Lights[0]->m_position);
+	ResourceManager::GetShader("basicLight").SetVector3f("light.ambient",	Lights[0]->m_ambient);
+	ResourceManager::GetShader("basicLight").SetVector3f("light.diffuse",	Lights[0]->m_diffuse);
+	ResourceManager::GetShader("basicLight").SetVector3f("light.specular",	Lights[0]->m_specular);
 	
 	// TODO: 设置 Enum 来规范 Category，Shader， Material 的名称
 	// TODO: 自动遍历并应用
 	// TODO：增加 Object 的 shader、texture、material 在 Init 中的初始化
 	// TODO：增加 shader、texture、material 在 Update 中的更改
-	/*		Category	Name			Shader			Texture			Material */
-	//ApplyTo("Player",	"player",		"default",		"default",			"default");
-	ApplyTo("Object",	"container",	"phong",		"container",	"phongMaterial");
-	ApplyTo("Object",	"floor",		"phong",		"wood",			"phongMaterial");
-	ApplyTo("Light",	"light",		"default",		"default",		"default");
+	/*		Category	Name			Shader			Texture */
+	ApplyTo("Object",	"container",	"basicLight",		"container");
+	ApplyTo("Object",	"floor",		"basicLight",		"wood");
+	ApplyTo("Light",	"light",		"default",			"default");
 }
 
 void Scene::ClearObject(std::string category, std::string name)
@@ -157,23 +137,12 @@ void Scene::ClearObjects()
 }
 
 
-void Scene::ApplyTo(std::string Category, std::string ObjectName, std::string Shader, std::string Texture, std::string Material)
+void Scene::ApplyTo(std::string Category, std::string ObjectName, std::string Shader, std::string Texture)
 {
-	if (Category == "Player") {
-		if (m_player == nullptr) {
-			std::cerr << "Warning: Player not found!" << std::endl;
-			return;
-		}
-		m_player->SetShader(Shader);
-		m_player->SetTexture(Texture);
-		m_player->SetMaterial(Material);
-		return;
-	}
 	if (Objects[Category].find(ObjectName) != Objects[Category].end())
 	{
 		Objects[Category][ObjectName]->SetShader(Shader);
 		Objects[Category][ObjectName]->SetTexture(Texture);
-		Objects[Category][ObjectName]->SetMaterial(Material);
 	}
 	else
 	{
@@ -197,8 +166,6 @@ void Scene::DrawObjects()
 void Scene::CheckClicked(double mouseX, double mouseY)
 {
 	float pre_depth = 1.0f;
-	bool checkAll = false; // 检查本次点击中是否有选中的
-	// TODO：增加功能，遍历完后只把深度最小的 AddClicked
 	for (auto& category : Objects) {
 		if (category.first == "player") continue;
 		for (auto& item : category.second) {
@@ -208,17 +175,9 @@ void Scene::CheckClicked(double mouseX, double mouseY)
 				item.second->SetClicked(true);
 				this->AddClicked(item.second);
 				pre_depth = item.second->GetDepth();
-				checkAll = true;
 			}
 		}
 	}
-
-// 现在修改为了右键取消选择
-	// 都没选中，说明点击的是空白位置，清空 m_chosenObject
-	//if (!checkAll && m_chosenObject) {
-	//	m_chosenObject->SetClicked(false);
-	//	m_chosenObject = nullptr;
-	//}
 }
 
 void Scene::AddClicked(Object* object)
