5f95757e21779dc6f2adf246428dcc50eaa5b530
ShinATK <soutakushin@gmail.com>
2024-03-08 11:09:32 +0800

    Update Light.cpp

--- src/scene/Light.cpp
+++ src/scene/Light.cpp
@@ -1,106 +1,75 @@
 #include "Light.h"
 
-Light::Light() : m_position(0.0f), m_color(1.0f), m_shown(false)
+Light::Light(glm::vec3 position, glm::vec3 color, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular) :
+    m_position(position), m_color(color), m_ambient(ambient), m_diffuse(diffuse), m_specular(specular), m_shown(true), m_size(0.5f), m_VAO(0)
 {
 
 }
 
-Light::~Light()
-{
-    //
+Light::~Light() {
+    glDeleteVertexArrays(1, &m_VAO);
 }
 
-void Light::Show()
+void Light::Draw(std::string new_shader)
 {
-	this->m_shown = true;
+    glm::mat4 model = glm::mat4(1.0f); // 一定记着初始化
+    model = glm::translate(model, m_position);
+    model = glm::scale(model, m_size);
+
+    ResourceManager::GetShader(new_shader).SetMatrix4("model", model);
+    ResourceManager::GetShader(new_shader).SetVector3f("color", m_color);
+    ResourceManager::GetShader(new_shader).Use();
+
+    this->BindVAO();
 }
 
-void Light::Hide()
+void Light::BindVAO()
 {
-	this->m_shown = false;
-}
+    // 顶点数据
+    glm::vec3 vertices[] = {
+        glm::vec3(-0.5f, -0.5f,  0.5f),
+        glm::vec3(0.5f, -0.5f,  0.5f),
+        glm::vec3(0.5f,  0.5f,  0.5f),
+        glm::vec3(-0.5f,  0.5f,  0.5f),
+        // 后面
+        glm::vec3(-0.5f, -0.5f, -0.5f),
+        glm::vec3(0.5f, -0.5f, -0.5f),
+        glm::vec3(0.5f,  0.5f, -0.5f),
+        glm::vec3(-0.5f,  0.5f, -0.5f)
+    };
+    unsigned int indices[] = {
+    0, 1, 2, 2, 3, 0,
+    1, 5, 6, 6, 2, 1,
+    7, 6, 5, 5, 4, 7,
+    4, 0, 3, 3, 7, 4,
+    3, 2, 6, 6, 7, 3,
+    4, 5, 1, 1, 0, 4
+    };
 
-void Light::SetPosition(glm::vec3 position)
-{
-	this->m_position = position;
-}
+    glGenVertexArrays(1, &m_VAO);
+    glBindVertexArray(m_VAO);
 
-void Light::SetPosition(float x, float y, float z)
-{
-	this->m_position = glm::vec3(x, y, z);
-}
+    unsigned int VBO, EBO;
 
-void Light::SetColor(glm::vec3 color)
-{
-	this->m_color = color;
-}
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-void Light::SetColor(float R, float G, float B)
-{
-	this->m_color = glm::vec3(R, G, B);
-}
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-void Light::RenderCube()
-{
-    if (this->m_shown)
-    {
-        // 顶点数据
-        glm::vec3 vertices[] = {
-            glm::vec3(-0.5f, -0.5f,  0.5f),
-            glm::vec3(0.5f, -0.5f,  0.5f),
-            glm::vec3(0.5f,  0.5f,  0.5f),
-            glm::vec3(-0.5f,  0.5f,  0.5f),
-            // 后面
-            glm::vec3(-0.5f, -0.5f, -0.5f),
-            glm::vec3(0.5f, -0.5f, -0.5f),
-            glm::vec3(0.5f,  0.5f, -0.5f),
-            glm::vec3(-0.5f,  0.5f, -0.5f)
-        };
-        unsigned int indices[] = {
-        0, 1, 2, 2, 3, 0,
-        1, 5, 6, 6, 2, 1,
-        7, 6, 5, 5, 4, 7,
-        4, 0, 3, 3, 7, 4,
-        3, 2, 6, 6, 7, 3,
-        4, 5, 1, 1, 0, 4
-        };
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-        glm::mat4 model = glm::mat4(1.0f); // 一定记着初始化
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
-        // Translate
-        model = glm::translate(model, m_position);
+    glBindVertexArray(0);
 
-        ResourceManager::GetShader("default").SetMatrix4("model", model);
+    glBindVertexArray(m_VAO);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
 
-        ResourceManager::GetShader("default").SetBoolean("bUseTexture", false);
-        ResourceManager::GetShader("default").SetVector3f("color", m_color);
-
-        unsigned int VBO, VAO, EBO;
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-        glGenBuffers(1, &EBO);
-
-        glBindVertexArray(VAO);
-
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-        // 设置顶点属性指针
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-
-        // 渲染立方体
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-
-        // 清理
-        glBindVertexArray(0);
-        glDeleteVertexArrays(1, &VAO);
-        glDeleteBuffers(1, &VBO);
-        glDeleteBuffers(1, &EBO);
-    }
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 }
 
