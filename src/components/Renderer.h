4c0dce07966c192f4bb9ce0f28fe1b500f0d9a16
ShinATK <soutakushin@gmail.com>
2024-03-09 11:11:04 +0800

    Update Renderer.h

--- src/components/Renderer.h
+++ src/components/Renderer.h
@@ -6,24 +6,31 @@ class Renderer
 {
 private:
 	GLuint m_FBO;
-	GLuint m_Texture;
+	GLuint m_DepthMap;
+	GLuint m_DepthCubemap;
 	GLuint m_quadVAO;
 	const GLuint SHADOW_WIDTH;
 	const GLuint SHADOW_HEIGHT;
+	glm::mat4 m_LightSpaceMatrix;
 public:
-	Renderer(const GLuint width, const GLuint height) : SHADOW_WIDTH(width), SHADOW_HEIGHT(height), m_quadVAO(0)
+	Renderer(const GLuint width, const GLuint height) : SHADOW_WIDTH(width), SHADOW_HEIGHT(height), m_quadVAO(0), m_LightSpaceMatrix(1.0f)
 	{
 		glGenFramebuffers(1, &m_FBO);
-		glGenTextures(1, &m_Texture);
+		glGenTextures(1, &m_DepthMap);
+		glGenTextures(1, &m_DepthCubemap);
 	}
 	~Renderer()
 	{
 		glDeleteFramebuffers(1, &m_FBO);
 	}
 
-	void InitShadowMap()
+	glm::mat4 GetLightSpaceMatrix() const { return this->m_LightSpaceMatrix; }
+
+	// Shadow Mapping ( Directional Light )
+	GLuint GetDepthMap() const { return this->m_DepthMap; }
+	void InitDepthMap()
 	{
-		glBindTexture(GL_TEXTURE_2D, m_Texture);
+		glBindTexture(GL_TEXTURE_2D, m_DepthMap);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -40,23 +47,87 @@ public:
 		glReadBuffer(GL_NONE);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	}
-
-	GLuint GetDepthMap() const { return this->m_Texture; }
-
-	void UpdateShadowMap()
+	void UpdateDepthMap(Shader& shader)
 	{
 		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
 		glBindFramebuffer(GL_FRAMEBUFFER, m_FBO);
 		glClear(GL_DEPTH_BUFFER_BIT);
-
+		glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 1.0f, 7.5f);
+		glm::mat4 lightView = glm::lookAt(glm::vec3(2.0f, 1.0f, 2.0f), glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		m_LightSpaceMatrix = lightProjection * lightView;
+		glCullFace(GL_FRONT); // 解决阴影悬浮问题
+		ResourceManager::GetShader("shadowMapping").SetMatrix4("lightSpaceMatrix", m_LightSpaceMatrix);
+		shader.SetMatrix4("lightSpaceMatrix", m_LightSpaceMatrix);
 	}
-	
-	void EndShadowMap(const GLuint SCR_WIDTH, const GLuint SCR_HEIGHT)
+	void EndDepthMap(const GLuint SCR_WIDTH, const GLuint SCR_HEIGHT)
+	{
+		glCullFace(GL_BACK);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+	void DebugDepthMap()
+	{
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, this->m_DepthMap);
+		ResourceManager::GetShader("shadowDebug").SetInteger("depthMap", 0);
+		ResourceManager::GetShader("shadowDebug").Use();
+		this->DebugShadowMap();
+	}
+
+
+	// Point shadows ( Point Light )
+	GLuint GetDepthCubemap() const { return this->m_DepthCubemap; }
+	void InitPointShadows()
+	{
+		glBindTexture(GL_TEXTURE_CUBE_MAP, m_DepthCubemap);
+		for (GLuint i = 0; i < 6; ++i)
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_DEPTH_COMPONENT,
+				SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, m_FBO);
+		glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, m_DepthCubemap, 0);
+		glDrawBuffer(GL_NONE);
+		glReadBuffer(GL_NONE);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+	void UpdateDepthCubemap(Shader& shader, glm::vec3 lightPos)
+	{
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, m_FBO);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		GLfloat aspect = (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT;
+		GLfloat near = 1.0f;
+		GLfloat far = 25.0f;
+		glm::mat4 shadowProj = glm::perspective(glm::radians(90.0f), aspect, near, far);
+		std::vector<glm::mat4> shadowTransforms;
+		shadowTransforms.push_back(shadowProj *
+			glm::lookAt(lightPos, lightPos + glm::vec3(1.0, 0.0, 0.0), glm::vec3(0.0, -1.0, 0.0)));
+		shadowTransforms.push_back(shadowProj *
+			glm::lookAt(lightPos, lightPos + glm::vec3(-1.0, 0.0, 0.0), glm::vec3(0.0, -1.0, 0.0)));
+		shadowTransforms.push_back(shadowProj *
+			glm::lookAt(lightPos, lightPos + glm::vec3(0.0, 1.0, 0.0), glm::vec3(0.0, 0.0, 1.0)));
+		shadowTransforms.push_back(shadowProj *
+			glm::lookAt(lightPos, lightPos + glm::vec3(0.0, -1.0, 0.0), glm::vec3(0.0, 0.0, -1.0)));
+		shadowTransforms.push_back(shadowProj *
+			glm::lookAt(lightPos, lightPos + glm::vec3(0.0, 0.0, 1.0), glm::vec3(0.0, -1.0, 0.0)));
+		shadowTransforms.push_back(shadowProj *
+			glm::lookAt(lightPos, lightPos + glm::vec3(0.0, 0.0, -1.0), glm::vec3(0.0, -1.0, 0.0)));
+		ResourceManager::GetShader("shadowCubemap")
+	}
+	void EndDepthCubemap(const GLuint SCR_WIDTH, const GLuint SCR_HEIGHT)
 	{
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
+
 
 	void DebugShadowMap()
 	{
